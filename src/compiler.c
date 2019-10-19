2db8c37c88a2a310b0b118118bd197445bcbceed
quantomistro <mahmoodsaifi38@gmail.com>
2019-10-19 18:45:54 +0800

    chp.17.3

--- src/compiler.c
+++ src/compiler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -7,9 +8,35 @@
 typedef struct {
     Token previous;
     Token current;
+    bool hadError;
+    bool panicMode;
 } Parser;
 
 Parser parser;
+
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+    return compilingChunk;
+}
+
+static void errorAt(Token* token, const char* message) {
+    if (parser.panicMode) return;
+    parser.panicMode = true;
+
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end.");
+    } else if (token->type == TOKEN_ERROR) {
+
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
+    }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
 
 static void errorAtCurrent(const char* message) {
     errorAt(&parser.current, message);
@@ -17,9 +44,16 @@ static void errorAtCurrent(const char* message) {
 
 bool compile(const char* source, Chunk* chunk) {
     initScanner(source);
+    compilingChunk = chunk;
+
+    parser.hadError = false;
+    parser.panicMode = false;
+
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
+    return !parser.hadError;
 }
 
 static void advance() {
@@ -31,4 +65,30 @@ static void advance() {
 
         errorAtCurrent(parser.current.start);
     }
+}
+
+static void consume(TokenType type, const char* message) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(message);
+}
+
+static void emitByte(uint8_t byte) {
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn() {
+    emitByte(OP_RETURN);
+}
+
+static void endCompiler() {
+    emitReturn();
 }
