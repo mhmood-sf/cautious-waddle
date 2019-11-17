a4fe2f24c036da4b15e5658e21462446846a621e
quantomistro <mahmoodsaifi38@gmail.com>
2019-11-17 16:28:27 +0800

    20.4 🎈

--- src/table.c
+++ src/table.c
@@ -21,11 +21,21 @@ void freeTable(Table* table) {
 
 Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
     uint32_t index = key->hash % capacity;
+    Entry* tombstone = NULL;
 
     for (;;) {
         Entry* entry = &entries[index];
 
-        if (entry->key == key || entry->key == NULL) {
+        if (entry->key == NULL) {
+            if (IS_NIL(entry->value)) {
+                // Empty entry
+                return tombstone != NULL ? tombstone : entry;
+            } else {
+                // We found a tombstone
+                if (tombstone == NULL) tombstone = entry;
+            }
+        } else if (entry->key == key) {
+            // We found the key.
             return entry;
         }
 
@@ -50,6 +60,7 @@ void adjustCapacity(Table* table, int capacity) {
         entries[i].value = NIL_VAL;
     }
 
+    table->count = 0;
     for (int i = 0; i < table->capacity; ++i) {
         Entry* entry = &table->entries[i];
         if (entry->key == NULL) continue;
@@ -57,6 +68,7 @@ void adjustCapacity(Table* table, int capacity) {
         Entry* dest = findEntry(entries, capacity, entry->key);
         dest->key = entry->key;
         dest->value = entry->value;
+        table->count++;
     }
 
     FREE_ARRAY(Entry, table->entries, table->capacity);
@@ -73,11 +85,25 @@ bool tableSet(Table* table, ObjString* key, Value value) {
     Entry* entry = findEntry(table->entries, table->capacity, key);
 
     bool isNewKey = entry->key == NULL;
-    if (isNewKey) table->count++;
+    if (isNewKey && IS_NIL(entry->value)) table->count++;
 
     entry->key = key;
     entry->value = value;
     return isNewKey;
+}
+
+bool tableDelete(Table* table, ObjString* key) {
+    if (table->count == 0) return false;
+
+    // Find the entry
+    Entry* entry = findEntry(table->entries, table->capacity, key);
+    if (entry->key == NULL) return false;
+
+    // Place tombstone
+    entry->key = NULL;
+    entry->value = BOOL_VAL(true);
+
+    return true;
 }
 
 void tableAddAll(Table* from, Table* to) {
